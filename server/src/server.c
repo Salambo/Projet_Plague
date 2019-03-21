8c69096707ffaf2c99d7b0ff5d5754786a4e48d2
Salomé <missmillka.@gmail.com>
2019-03-21 16:47:54 +0100

    Auto stash before merge of "dev" and "origin/dev"

--- server/src/server.c
+++ server/src/server.c
@@ -1,1 +1,26 @@
 #include "server.h"
+
+Building ** MemoryAllocationCity(){
+
+  int i;
+
+  Building ** city= (int **)malloc(sizeof(int *)*7);
+
+  if(city==NULL){
+    printf("Erreur d'allocation de mémoire \n");
+
+  }
+  else{
+    for(i=0; i<7; i++){
+      city[i]= (int *)malloc(sizeof(int)*hauteur);
+    }
+  }
+
+  return city; 
+}
+
+Building ** CityInitialization(){
+
+    
+
+}
